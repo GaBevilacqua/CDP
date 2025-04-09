6ad50337376aa43a16f82a340b869911c266cefd
GaBevilacqua <gabriel.bevilacqua.2002@gmail.com>
2025-04-09 18:51:03 -0300

    1

--- Atividade1/Produtor.py
+++ Atividade1/Produtor.py
@@ -3,7 +3,22 @@ import struct
 from xml.etree import ElementTree as ET
 import time
 
-curr = time.ctime(1744232821.0800228)
-print(curr)
-print(time.gmtime(0),"\n")
-print(time.sleep(0))
+def createPXML(id,name, place, year):
+    #Criando uma pessoa e atribuindo um identficador a ela
+    pessoa = ET.Element('pessoa')
+    pessoa.set('id', str(id))
+
+    #Toda pessoa tem um nome, lugar e ano
+    ET.SubElement(pessoa, 'nome').text = name
+    ET.SubElement(pessoa, 'lugar').text = place
+    ET.SubElement(pessoa, 'ano').text = year
+
+    return ET.tostring(pessoa, encoding='utf-8')
+
+def multicast_produtor():
+    MULTICAST_GROUP = '224.1.1.1'
+    PORT = 5007
+    TTL = 1
+
+    sock = socket.socket(socket.AL_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL,TTL)
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # CDP
+
+## Exercício 1
+
+### Base
+Comunicação Difusa Seletiva (Multicast), 1 host manda para vários destinatários (n), nesse caso usamos UDP.
+Para a serialização dos dados utilizamos XML, podemos usar as bibliotecas(xml.etree.ElementTree)
+
+### Arquitetura do Programa Produtos
+
+Configuração do socket (UDP,IP, Port, TTL) -> Ciração XML -> Serialização -> Envio
+
+### Arquitetura Consumidor
+COnfiguração do socket -> Recepção -> Desserialização
+
+
+
+
